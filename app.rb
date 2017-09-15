1db82ef0d7337739892230e5c339305401eef464
Hayden Betts <haydenbetts@gmail.com>
2017-09-15 02:46:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+
+		# this tells sinatra to render a file called index.erb inside of a
+		# directory called views... ok so I guess the erb function is built in
+		# to sinatra and it only looks for its argument in the ../views folder
+		erb :index
 	end
+
+	get "/info" do
+       erb :info
+  end
+
 end
